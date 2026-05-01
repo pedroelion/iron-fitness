a7be737c80c8cef4e52f489f03a9b908515a50df
Pedro Elion <pedroelion.aranha@gmail.com.br>
2026-05-01 02:15:07 -0300

    Solucao definitiva com go embed

--- api/index.go
+++ api/index.go
@@ -1,25 +1,23 @@
 package handler
 
 import (
+	_ "embed"
 	"html/template"
 	"modulo/modelo"
 	"net/http"
-	"path/filepath"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	// Caminho para encontrar o seu HTML na Vercel
-	caminhoTemplate := filepath.Join("templates", "index.html")
+//go:embed index.html
+var htmlContent string
 
-	temp, err := template.ParseFiles(caminhoTemplate)
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// O Go lê o HTML diretamente da variável embutida
+	temp, err := template.New("index").Parse(htmlContent)
 	if err != nil {
 		http.Error(w, "Erro ao carregar o site: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// Puxa os dados da academia lá da sua pasta modelo
 	dados := modelo.GetPageData()
-
-	// Mostra o HTML na tela
 	temp.Execute(w, dados)
 }
